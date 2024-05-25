86043635810799554a6929482fd2736d040e384e
Alexandr <isbarius@yandex.ru>
2024-05-25 19:31:12 +0300

    Changes -1

--- src/test/java/ru/netology/web/page/VerificationPage.java
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -1,22 +1,21 @@
-package page;
+package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
-import data.DataHelper;
+import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class VerificationPage {
-    private SelenideElement codeField = $("[data-test-id=code] input");
-    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
 
-    public VerificationPage() {
+            private final SelenideElement codeField = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
 
+    public VerificationPage(){
         codeField.shouldBe(visible);
     }
 
-    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
-
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode){
         codeField.setValue(verificationCode.getCode());
         verifyButton.click();
         return new DashboardPage();
---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,58 @@
+package ru.netology.web.test;
+
+import lombok.val;
+import lombok.var;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.web.data.DataHelper.*;
+
+public class MoneyTransferTest {
+    DashboardPage dashboardPage;
+    CardInfo firstCardInfo;
+    CardInfo secondCardInfo;
+    int firstCardBalance;
+    int secondCardBalance;
+
+    @BeforeEach
+    void setup() {
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = getVerificationCode();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+        firstCardInfo = getFirstCardInfo();
+        secondCardInfo = getSecondCardInfo();
+        firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+    }
+
+    @Test
+    void shouldTransferFromFirstToSecond() {
+        val amount = generateValidAmount(firstCardBalance);
+        val expectedBalanceFirstCard = firstCardBalance - amount;
+        val expectedBalanceSecondCard = secondCardBalance + amount;
+        val transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        val actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        val actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+
+    @Test
+    void shouldGetErrorMessageIfAmountMoreBalance() {
+        val amount = generateValidAmount(secondCardBalance);
+        val transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
+        transferPage.findErrorMessage("Выполнена попытка перевода суммы, превышающей остаток на карте списания");
+        val actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        val actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(firstCardBalance, actualBalanceFirstCard);
+        assertEquals(secondCardBalance, actualBalanceSecondCard);
+    }
+}
