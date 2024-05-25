38af1dcad76bf0b641d31fca7c182aedc9671fae
Alexandr <isbarius@yandex.ru>
2024-05-25 18:07:19 +0300

    Add Tests

---
+++ src/test/java/page/LoginPage.java
@@ -0,0 +1,22 @@
+package page;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import data.DataHelper;
+
+import java.util.Calendar;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordFieled = $("[data-test-id=password] input");
+    private SelenideElement loginButton = $("[data-test-id=action-login]");
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordFieled.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+
+}
